3163bb887b43c89eddb43fcf15fb477680a453f5
Do Huan <huanyd1@gmail.com>
2020-12-17 11:57:55 +0700

    delete js

--- user/js/khoa.js
+++ user/js/khoa.js
@@ -5,16 +5,3 @@ function InvalidMsg(textbox) {
     }
     return true;
 }
-
-function normalImg(textbox) {
-    alert("Chuột đã rời đi");
-}
-
-function normalImg1(textbox) {
-    alert("Chuột đã rời đi 2");
-}
-
-function checkinput(textbox) {
-
-        alert("Trùng kìa");
-}
